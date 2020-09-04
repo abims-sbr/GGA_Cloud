f5acdbf7f2adb7fa4b3b4cb0421aa9bba10311de
Romain Dallet <romain.dallet@sb-roscoff.fr>
2020-09-04 10:05:37 +0200

    Fix playbook genocloud ssh connection

--- terraform/resource.tf
+++ terraform/resource.tf
@@ -1,7 +1,9 @@
-resource "openstack_compute_instance_v2" "test" {
-    name            = "test-vm"
-    image_name      = "Debian9+Docker"
+resource "openstack_compute_instance_v2" "GGA" {
+    name            = "GGA"
+    image_name      = "Debian10"
     flavor_name     = "m1.medium"
+    key_pair        = "local-rdallet"
+    security_groups = ["default"]
 
     network {
         name = var.tenant_network
