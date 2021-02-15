b10c3c9966b141db3d11c82b8cd2f1a3dd868eeb
RomainDallet <romain.dallet@sb-roscoff.fr>
2021-02-15 09:40:36 +0100

    Generate template for terraform role

--- roles/gga_terraform/templates/outputs.tf
+++ roles/gga_terraform/templates/outputs.tf
@@ -3,7 +3,7 @@ resource "local_file" "AnsibleInventory" {
  content = templatefile("hosts.tmpl",
  {
   os_cloud_ip = var.os_auth_url,
-  os_vms_ip = openstack_compute_instance_v2.GGA.access_ip_v4,
+  os_vms_ip = "openstack_compute_instance_v2.{{ vm_name }}.access_ip_v4",
  }
  )
  filename = "hosts"
